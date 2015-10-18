221f7e7d5c85f6896e0d76dbf0ae8102f21e2a57
clean <clean@Johns-MacBook-Air.local>
2015-10-18 01:31:14 -0400

    added user sign in, save session, destroy session, pull down menu, fixed links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,28 @@ class UsersController < ApplicationController
   def new
 	@user = User.new
   end
+
  def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-	flash[:success] = "Welcome to the Batphone App!"
+	flash[:success] = "Welcome to the Bat-Phone Web App!"
       redirect_to @user
     else
       render 'new'
+    end
+ end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
