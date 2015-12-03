ea1faa2c5d961f40881520bd9ae7b55486855e31
clean <clean@Johns-MacBook-Air.local>
2015-12-03 14:32:32 -0500

    ffu

--- config/routes.rb
+++ config/routes.rb
@@ -70,41 +70,18 @@ Rails.application.routes.draw do
   end
 
 
+  get :startaudiostream, controller:"streams"
 
-  def Stream6Broadcasting
-    @network = Network.first
+  get :startlistener, controller:"streams"
 
+  get :tunnelToMasterFrom6, controller:"streams"
 
-    @stream = Stream.new(:network_id => 1, :port => "18886", :device_id => 6)
-    `ssh -n -f -vvv -p 2222 -R 18880:root@192.168.2.6:22 root@129.10.248.216 "arecord -f cd -Dhw:sndrpiwsp | ffmpeg -fflags nobuffer -i - -acodec libmp3lame -q:a 6 -ac 2 -re -f mpegts udp://192.168.2.6:1234"`
-    flash[:success] = "Broadcasting stream created at Recording Pi Node 6!"
+  get :Stream6Broadcasting, controller:"streams"
 
-    @stream = Stream.new(:network_id => 1, :port => "18886", :device_id => 6)
+  get :ForwardStreamThroughNode, controller:"streams"
 
-    redirect_to @network
+  get :CreateStreamDeviceReceiving, controller:"streams"
 
-  end
-
-  def ForwardStreamThroughNode
-    `ssh -n -f -vvv -p 2222 -R 18881:root@192.168.2.6:22 root@129.10.248.216 " ffmpeg -i udp://192.168.2.7:1234 -acodec copy -f mpegts udp://192.168.2.8:1234`
-    flash[:success] = "Forwarding audio stream from Recording Pi .7 to Recording Pi .8"
-
-    @network = Network.first
-    @stream = Stream.new(:network_id => 1, :port => "18886", :device_id => 6)
-
-    redirect_to @network
-  end
-
-
-  def CreateStreamDeviceRecieving
-    `ssh -n -f -vvv -p 2222 -R 18880:root@192.168.2.6:22 root@129.10.248.216 " ffplay -fflags nobuffer -nodisp -f mpegts -i udp://192.168.2.6:1234`
-    flash[:success] = "Recording Pi 8 is now recieving the audio stream"
-
-    @network = Network.first
-    @stream = Stream.new(:network_id => 1, :port => "18886", :device_id => 6)
-
-    redirect_to @network
-  end
 
 
 
