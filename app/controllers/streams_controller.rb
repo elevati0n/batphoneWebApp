2a621fcb0078acec42cd3116e539c144fb3c7e03
clean <clean@Johns-MacBook-Air.local>
2015-12-03 08:24:47 -0500

    asd

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -15,6 +15,8 @@ class StreamsController < ApplicationController
 #      `ssh -n -f -vvv -p 2222 -L 18886:192.168.2.6:22 root@129.10.248.216
  #         "sudo ffplay -nodisp -fflags nobuffer -f mpegts -i tcp://104.131.44.2:18882"`
       #`ssh -vvv -p 18886 localhost `
+      `ssh -n -f -vvv -p 2222 -L 18886:root@192.168.2.6:22 root@129.10.248.216 "sudo ffplay -nodisp -fflags nobuffer -f mpegts -i tcp://104.131.44.2:18882 &"`
+
       redirect_to @stream
     end
 
@@ -24,8 +26,6 @@ class StreamsController < ApplicationController
   def show
 
     @stream = Stream.find(params[:id])
-    `ssh -n -f -vvv -p 2222 -L 18886:root@192.168.2.6:22 root@129.10.248.216 "sudo ffplay -nodisp -fflags nobuffer -f mpegts -i tcp://104.131.44.2:18882 &"`
-
 
 
     end
@@ -61,4 +61,3 @@ private
   end
 
 
-end
