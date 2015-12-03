6514811122e5cd03230081dcb03c35e77aed51a5
sean <sean@capstone>
2015-12-03 01:35:30 -0500

    added db controller

---
+++ app/helpers/dbmanage_helper.rb
@@ -0,0 +1,2 @@
+module DbmanageHelper
+end
--- public/manage_database.rb
+++ public/manage_database.rb
@@ -1,33 +1,76 @@
     timestamp="Tue Nov  26 16:02:31 EST 2015"
 
+    
+# Open JSON file
+  File.open("public/database_migration.json", 'r') do |f|
+# Parse the JSON file
+        f.each do |line|
+          json = JSON.parse(line)
+                json.each do |hash|
+#puts hash
+                        hash.keys.each do |key|
+#puts key
+                                hash[key].each do |inner_hash|
+#                               key.to_a
+                                @temp = Object.const_get(key).new
+#                               @temp = key.new
+
+                                        inner_hash.keys.each do |inner_key|
+puts inner_hash
+#puts inner_hash[inner_key]
+puts "before it"
+#                                       @temp.send(inner_key) = inner_hash[inner_key]
+                                        @temp.update_attributes(inner_hash)
+puts "after it"
+#                                       puts inner_hash[inner_key]
+# puts                  inner_hash
+                                        end
+                                @temp.save
+                                end
+                        end
+                end
+#                 puts instance_attribute
+#                end
+         end
+
+
+
     #Create an array of models
-    #models = ["User", "Device", "Group","Network","Micropost","Comment","Relationship","Recording"]
+    models = [User, Device,Group,Network,Micropost,Comment,Relationship,Recording]
+    models.to_a
+    #Create an array for storing json data
+    final_json = []
     #Iterate through each model
-    #models.each do |model|
-        #Create an array for storing json data  
-        events_json=[]
-        #Create an array of attributes
-        model_attributes = User.column_names
-	puts model_attributes
+    models.each do |model|
+        # Create an array of attributes
+        model_attributes = model.column_names
+	# Create an array for a specific model
+	events_json = []
+	# Initiate a json array
+	json_model = {}
         # iterate inside each model
-        User.all.each do |event|
-            #compare the timestamp to figure out if its new:
+        model.all.each do |event|
+            # compare the timestamp to figure out if its new:
             if event.updated_at > timestamp
-		puts event
-                #insert the current model instance into the teporary json array  
+                # insert the current model instance into the teporary json array  
                 event_json = {}
-                model_attributes.each do |model_attribute|
-               	     puts model_attribute
-		     puts event.send(model_attribute)
-		     event_json[model_attribute]=event.send(model_attribute)
+               # Iterate through each attribute of model instances and store them into a json array
+		 model_attributes.each do |model_attribute|
+          	     event_json[model_attribute]=event.send(model_attribute)
+#                local_model = event.to_json	
 		end
+		# Store each model instance( e.g. user) into json array
 		 events_json << event_json
-#		events_json << event_json
             end
         end
-   # end
+#  	puts events_json
+		json_model[model]=events_json
+                 final_json << json_model
+
+    end
    #write model json to the actual json file
     File.open("public/database_migration.json","w") do |f|
-        f.write(events_json.to_json)
+        f.write(final_json.to_json)
     end 
-    
+
+  end
