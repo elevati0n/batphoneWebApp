98ceddf7c5b59b01e994d8f779c4fd8e71730fd5
clean <clean@Johns-MacBook-Air.local>
2015-12-03 05:47:47 -0500

    stream functions

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -12,6 +12,8 @@ class StreamsController < ApplicationController
   def create
     @stream = Stream.new(stream_params)
     if @stream.save
+      `ssh -vvv -p 2222 -L 18886:192.168.2.6:22 root@129.10.248.216`
+      `ssh -vvv -p 18886 localhost `
       redirect_to @stream
     end
 
